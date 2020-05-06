7fdd0dfdf0d35a7cb6dcf346b80aab95c8410bff
Kabirdas Henry <kbrdsk@gmail.com>
2020-05-05 17:38:58 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				use: [
+					'file-loader',
+				],
+			},
+		],
+	},
+};
