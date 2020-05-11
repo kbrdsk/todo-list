ecd1b7dd215993623b5ecb04d1c2dc7c7ae5c7c5
Kabirdas Henry <kbrdsk@gmail.com>
2020-05-11 08:57:57 -0700

    removed test interface, added dom elements for nav menu

--- src/index.js
+++ src/index.js
@@ -1,97 +1,57 @@
 import {todo, project, category, contact} from './todo.js';
+import './styles/style.css';
 
-let activeObj;
+let projects = new Set(),
+	categories = new Set(),
+	contacts = new Set(),
+	todos = new Set();
 
-let todoCounter = 0,
-	projCounter = 0,
-	catCounter = 0,
-	contactCounter = 0;
-
-let body = document.querySelector('body');
-body.style.display = 'flex';
-body.style.flexDirection = 'column';
-
-let activeObjDisplay = document.createElement('h1');
-body.appendChild(activeObjDisplay);
-
-let todolist = document.createElement('button'),
-	tags = document.createElement('button'),
-	self =  document.createElement('button');
-
-todolist.textContent = 'todoList';
-tags.textContent = 'tags';
-self.textContent = 'self';
-
-todolist.addEventListener('click', () => {
-	console.log(activeObj.todoList.list());
-});
-
-tags.addEventListener('click', () => {
-	console.log(activeObj.tags.list());
-});
-
-self.addEventListener('click', () => {
-	console.log(activeObj);
-});
+let header = document.querySelector('header'),
+	footer = document.querySelector('footer'),
+	main = document.querySelector('main');
 
 
-for(let button of [todolist, tags, self]){
-	body.appendChild(button);
+
+//nav menu
+
+let navButton = document.createElement('button'),
+	navMenu = document.createElement('div'),
+	editLink = document.createElement('a'),
+	categoriesLink = document.createElement('a'),
+	projectsLink = document.createElement('a'),
+	contactsLink = document.createElement('a');
+
+navButton.classList.add('nav-button');
+
+navMenu.classList.add('nav-menu');
+navMenu.setAttribute('showing', 'false');
+
+editLink.classList.add('nav-link');
+editLink.textContent = 'Edit';
+
+categoriesLink.classList.add('nav-link');
+categoriesLink.textContent = 'Categories';
+
+projectsLink.classList.add('nav-link');
+projectsLink.textContent = 'Projects';
+
+contactsLink.classList.add('nav-link');
+contactsLink.textContent = 'Contacts';
+
+navMenu.appendChild(editLink);
+navMenu.appendChild(projectsLink);
+navMenu.appendChild(categoriesLink);
+navMenu.appendChild(contactsLink);
+
+navButton.addEventListener('click', openNavMenu);
+
+header.appendChild(navButton);
+header.appendChild(navMenu);
+
+function openNavMenu(){
+	navMenu.setAttribute('showing', 'true');
 }
 
-let todoButton = document.createElement('button'),
-	projButton = document.createElement('button'),
-	catButton = document.createElement('button'),
-	contactButton = document.createElement('button');
-
-todoButton.textContent = 'new todo';
-projButton.textContent = 'new project';
-catButton.textContent = 'new category';
-contactButton.textContent = 'new contact';
-
-for(let button of [todoButton, projButton, catButton, contactButton]){
-	body.appendChild(button);
-}
-
-todoButton.addEventListener('click', () => {
-	let title = `todo ${todoCounter++}`;
-	createTestButton(todo(title), title);
-});
-
-projButton.addEventListener('click', () => {
-	let title = `proj ${projCounter++}`;
-	createTestButton(project(title), title);
-});
-
-catButton.addEventListener('click', () => {
-	let title = `cat ${catCounter++}`;
-	createTestButton(category(title), title);
-});
-
-contactButton.addEventListener('click', () => {
-	let title = `contact ${contactCounter++}`;
-	createTestButton(contact(title), title);
-});
-
-function createTestButton(object, title){
-	let button = document.createElement('button');
-	button.obj = object;
-	button.textContent = title;
-	button.style.margin = '10px';
-	let addButton = document.createElement('button');
-	addButton.obj = object;
-	addButton.textContent = 'add';
-	button.addEventListener('click', e => activateObj(object, title));
-	addButton.addEventListener('click', e => addToActive(object));
-	body.appendChild(button);
-	body.appendChild(addButton);
-}
-
-function activateObj(object, title){
-	activeObj = object;
-	activeObjDisplay.textContent = title;
-}
-
-function addToActive(object){
-	activeObj.todoList.add(object);
+function closeNavMenu(){
+	navMenu.setAttribute('showing', 'false');
 }
